4f0fe97036d5f6274ac7d1a138573fb53ceabddc
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-12 16:34:48 -0700

    Add request, write middleware and route to render bonuses.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
-const port = 3033;
+const parser = require('body-parser');
+const request = require('request');
+const morgan = require('morgan');
+const port = process.env.PORT || 3033;
+const bonusUrl = 'http://localhost:3031/'
 
 app.use(morgan('dev'));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+// Send header to allow all cross origin requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
+})
+
+app.get('/', (req, res) => {
+  request(bonusUrl).pipe(res);
 })
