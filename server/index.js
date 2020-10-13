318177ce9290f74987883066332acfa7226ae763
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-12 20:05:43 -0700

    Successfully render one basic service template

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
+const path = require('path');
 const request = require('request');
 const morgan = require('morgan');
 const port = process.env.PORT || 3033;
-const bonusUrl = 'http://localhost:3031/'
+//const bonusUrl = 'http://localhost:3031'
 
 app.use(morgan('dev'));
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
+
+//need to get static files serving
+app.use('/', express.static('client/src'));
 
 // Send header to allow all cross origin requests
 app.use((req, res, next) => {
@@ -21,6 +25,12 @@ app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
 })
 
+// This should become unneccessary when express static works
 app.get('/', (req, res) => {
-  request(bonusUrl).pipe(res);
+  res.sendFile('/Users/kimmybeee/Desktop/kimarie-proxy/client/index.html');
+})
+
+app.get('/bonuses/bundle.js', (req, res) => {
+  console.log('Worked!');
+  request('http://localhost:3031/bundle.js').pipe(res);
 })
