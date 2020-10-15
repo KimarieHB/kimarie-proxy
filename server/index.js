4bae5d3cb8e9dc309550904da33427c90e989df3
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-14 17:49:26 -0700

    Fix bugs to successfully serve static files
    without a work-around, tidy up file structure, add error handling to get requests to services

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
-const path = require('path');
 const request = require('request');
 const morgan = require('morgan');
 const port = process.env.PORT || 3033;
@@ -10,10 +9,9 @@ app.use(morgan('dev'));
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
-//need to get static files serving
-app.use('/', express.static('client/src'));
+app.use(express.static('./client'));
 
-// Send header to allow all cross origin requests
+// Send header to allow all cross origin requests; header may need to be more specific in production
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -22,26 +20,54 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
-})
-
-// This should become unneccessary when express static works
-app.get('/', (req, res) => {
-  res.sendFile('/Users/kimmybeee/Desktop/kimarie-proxy/client/index.html');
 });
 
 app.get('/bonuses/bundle.js', (req, res) => {
-  console.log('Worked!');
-  request('http://localhost:3031/bundle.js').pipe(res);
+  request('http://localhost:3031/bundle.js')
+  .on('error', (err) => {
+    let msg = 'Error on connection to BONUSES';
+    console.log(msg, err);
+    res.status(500).send(msg);
+  })
+  .pipe(res);
 });
 
 app.get('/items/bundle.js', (req, res) => {
-  request('http://localhost:3200/bundle.js').pipe(res);
+  request('http://localhost:3200/bundle.js')
+  .on('error', (err) => {
+    let msg = 'Error on connection to ITEMS';
+    console.log(msg, err);
+    res.status(500).send(msg);
+  })
+  .pipe(res);
 });
 
 app.get('/tiers/bundle.js', (req, res) => {
-  request('http://localhost:3101/bundle.js').pipe(res);
+  request('http://localhost:3101/bundle.js')
+  .on('error', (err) => {
+    let msg = 'Error on connection to TIERS';
+    console.log(msg, err);
+    res.status(500).send(msg);
+  })
+  .pipe(res);
 });
 
 app.get('/descriptions/bundle.js', (req, res) => {
-  request('http://localhost:3663/bundle.js').pipe(res);
+  request('http://localhost:3663/bundle.js')
+  .on('error', (err) => {
+    let msg = 'Error on connection to DECRIPTIONS';
+    console.log(msg, err);
+    res.status(500).send(msg);
+  })
+  .pipe(res);
+});
+
+app.get('/charity/bundle.js', (req, res) => {
+  request('http://localhost:3987/bundle.js')
+  .on('error', (err) => {
+    let msg = 'Error on connection to CHARITIES';
+    console.log(msg, err);
+    res.status(500).send(msg);
+  )}
+  .pipe(res);
 });
