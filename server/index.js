32dea6c0ca1fb054c62a018ca901195a9155707d
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-24 00:16:02 -0700

    Update to simplified proxy

--- server/index.js
+++ server/index.js
@@ -11,84 +11,22 @@ app.use(parser.json());
 
 app.use(express.static('./client'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', '*');
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Headers', '*');
+//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
 });
 
-app.get('/7', (req, res) => {
-  //'/Users/kimmybeee/Desktop/kimarie-proxy/client/index.html'
-  res.sendFile('/Users/kimmybeee/Desktop/kimarie-proxy/client/index.html', (err) => {
-    if (err) {
-      let msg = 'Error on connection to Proxy HTML';
-      console.log(msg, err);
-      res.send(err);
-    }
-  })
-})
-
-app.get('/bonus/:id', (req, res) => {
-  request('http://localhost:3031/bonus/:id')
-  .on('error', (err) => {
-    let msg = 'Error on connection to BONUSES';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
-app.get('/bonuses/bundle.js', (req, res) => {
-  request('http://localhost:3031/bundle.js')
-  .on('error', (err) => {
-    let msg = 'Error on connection to BONUSES';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
-app.get('/items/bundle.js', (req, res) => {
-  request('http://localhost:3200/bundle.js')
-  .on('error', (err) => {
-    let msg = 'Error on connection to ITEMS';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
-app.get('/tiers/bundle.js', (req, res) => {
-  request('http://localhost:3101/bundle.js')
-  .on('error', (err) => {
-    let msg = 'Error on connection to TIERS';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
-app.get('/descriptions/bundle.js', (req, res) => {
-  request('http://localhost:3663/bundle.js')
-  .on('error', (err) => {
-    let msg = 'Error on connection to DECRIPTIONS';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
-app.get('/charity/bundle.js', (req, res) => {
-  request('http://localhost:3987/bundle.js')
-  .on('error', (err) => {
-    let msg = 'Error on connection to CHARITIES';
-    console.log(msg, err);
-    res.status(500).send(msg);
-  })
-  .pipe(res);
-});
-
+// app.get('/*', (req, res) => {
+//   request('http://localhost:3031')
+//   .on('error', (err) => {
+//     let msg = 'Error on connection to BONUSES';
+//     console.log(msg, err);
+//     res.status(500).send(msg);
+//   })
+//   .pipe(res);
+// });
